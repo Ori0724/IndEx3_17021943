d9307704825a5dc91fe156cb6ad3721bf7359e84
Ori0724 <Ori0724@users.noreply.github.com>
2020-02-05 16:26:10 +0000

    add ...

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from config import DevConfig
+
+db = SQLAlchemy()
+
+
+def create_app(config_class=DevConfig):
+    """
+    Creates an application instance to run using settings from config.py
+    :return: A Flask object
+    """
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialise the database and create tables
+
+
+db.init_app(app)
+from app.models import Teacher, Student, Course, Grade
+
+with app.app_context():
+    db.create_all()
+
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+
+    return app
--- run.py
+++ run.py
@@ -10,3 +10,8 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
+from app import create_app
+
+app = create_app()
+app.app_context().push()
